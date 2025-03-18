3ca2b9c42d416149b0786aee3932ee47319181e4
choe-yujin <dev.yujinchoe@gmail.com>
2025-03-18 21:52:40 +0900

    refactor: cutter가 ProcessingResult를 반환하도록 수정

--- src/main/java/com/devyujin/mission/service/FruitCutter.java
+++ src/main/java/com/devyujin/mission/service/FruitCutter.java
@@ -8,7 +8,10 @@ public class FruitCutter<T extends Cuttable> {
         this.fruit = fruit;
     }
 
-    public String cut() {
-        return fruit.cut();
+    /*    처리기 클래스들이 ProcessingResult를 반환하도록 수정한다.
+    과일의 상태(예: 바나나의 익은 정도, 사과의 색상)에 따라 다른 처리 결과를 반환하도록 구현한다.*/
+    public ProcessingResult<T> cut() {
+        String result = fruit.cut(); // peel 메서드는
+        return new ProcessingResult<>(fruit, result); // 과일 상태에 따라 다른 결과 반환
     }
 }
